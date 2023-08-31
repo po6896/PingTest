b10527a8e4f95c76bc3f9a9eba0c1cac0fcef15e
po6896 <119492836+po6896@users.noreply.github.com>
2023-08-31 14:18:09 +0900

    Update PingTest.bat
    IPchange

--- PingTest.bat
+++ PingTest.bat
@@ -15,7 +15,7 @@ REM ここからバッチファイルの処理を記述
 REM echo このバッチファイルは管理者として実行されています。
 
 REM Pingを送信するIPアドレス
-set "targetIP=www.google.com"
+set "targetIP=127.0.0.1"
 
 REM Pingを送信し、応答があるか確認
 ping -n 1 %targetIP% > nul
@@ -28,7 +28,7 @@ if %errorlevel%==0 (
 )
 
 REM Pingを送信するIPアドレス
-set "targetIP=www.yahoo.co.jp"
+set "targetIP=8.8.8.8"
 
 REM Pingを送信し、応答があるか確認
 ping -n 1 %targetIP% > nul
@@ -41,7 +41,7 @@ if %errorlevel%==0 (
 )
 
 REM Pingを送信するIPアドレス
-set "targetIP=192.168.1.1"
+set "targetIP=1.1.1.1"
 
 REM Pingを送信し、応答があるか確認
 ping -n 1 %targetIP% > nul
@@ -54,7 +54,7 @@ if %errorlevel%==0 (
 )
 
 REM Pingを送信するIPアドレス
-set "targetIP=0.0.0.0"
+set "targetIP=1.0.0.1"
 
 REM Pingを送信し、応答があるか確認
 ping -n 1 %targetIP% > nul
