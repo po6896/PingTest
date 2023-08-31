460fdb3d437f4aed0c827158ca98ed097e357f26
po6896 <119492836+po6896@users.noreply.github.com>
2023-08-31 17:16:00 +0900

    Update PingTest.bat
    design and setlocal change

--- PingTest.bat
+++ PingTest.bat
@@ -1,75 +1,26 @@
 @echo off
-setlocal
-REM 疎通チェックバッチファイル
-REM 対象 DB WEB Engine x 2 のIP
+setlocal enabledelayedexpansion
 
-REM 管理者として実行されているかチェック
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo このスクリプトを実行するには管理者権限が必要です。
-    pause
-    exit
+echo --------------------------------------------------------------
+echo IPアドレス        サーバー名              状態
+echo --------------------------------------------------------------
+
+for %%i in (
+    "127.0.0.1" "DBサーバー" "8.8.8.8" "WebAPサーバー"
+    "1.1.1.1" "Engineサーバー#1" "1.0.0.1" "Engineサーバー#2"
+) do (
+    set "targetIP=%%~i"
+    ping -n 1 !targetIP! > nul
+
+    if !errorlevel! equ 0 (
+        set "status=オンライン"
+    ) else (
+        set "status=オフラインか応答遅延"
+    )
+
+    echo !targetIP!        %%~j  !status!
 )
 
-REM ここからバッチファイルの処理を記述
-REM echo このバッチファイルは管理者として実行されています。
-
-REM Pingを送信するIPアドレス
-set "targetIP=127.0.0.1"
-
-REM Pingを送信し、応答があるか確認
-ping -n 1 %targetIP% > nul
-
-REM 変数xの値に応じてメッセージを表示
-if %errorlevel%==0 (
-    echo 応答がありました。DBサーバー アドレス %targetIP% はオンラインです.
-) else (
-    echo 応答がありませんでした。サーバー IPアドレス %targetIP% はオフラインか応答が遅い可能性があります.
-)
-
-REM Pingを送信するIPアドレス
-set "targetIP=8.8.8.8"
-
-REM Pingを送信し、応答があるか確認
-ping -n 1 %targetIP% > nul
-
-REM 変数xの値に応じてメッセージを表示
-if %errorlevel%==0 (
-    echo 応答がありました。WebAPサーバー IPアドレス %targetIP% はオンラインです.
-) else (
-    echo 応答がありませんでした。WebAPサーバー IPアドレス %targetIP% はオフラインか応答が遅い可能性があります.
-)
-
-REM Pingを送信するIPアドレス
-set "targetIP=1.1.1.1"
-
-REM Pingを送信し、応答があるか確認
-ping -n 1 %targetIP% > nul
-
-REM 変数xの値に応じてメッセージを表示
-if %errorlevel%==0 (
-    echo 応答がありました。Engineサーバー#1 IPアドレス %targetIP% はオンラインです.
-) else (
-    echo 応答がありませんでした。Engineサーバー#1 IPアドレス %targetIP% はオフラインか応答が遅い可能性があります.
-)
-
-REM Pingを送信するIPアドレス
-set "targetIP=1.0.0.1"
-
-REM Pingを送信し、応答があるか確認
-ping -n 1 %targetIP% > nul
-
-REM 変数xの値に応じてメッセージを表示
-if %errorlevel%==0 (
-    echo 応答がありました。Engineサーバー#2 IPアドレス %targetIP% はオンラインです.
-) else (
-    echo 応答がありませんでした。Engineサーバー#2 IPアドレス %targetIP% はオフラインか応答が遅い可能性があります.
-)
-
-REM 何かメッセージを表示
-REM echo キーを押してください.
-
-REM ユーザーがキーを押すのを待機
+echo --------------------------------------------------------------
 pause
-
 endlocal
