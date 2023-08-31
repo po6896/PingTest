aeac211f1d23b31d9f955fc7ff17893cef4b268e
po6896 <119492836+po6896@users.noreply.github.com>
2023-08-31 18:13:40 +0900

    Update PingTest.bat
    design fix

--- PingTest.bat
+++ PingTest.bat
@@ -1,26 +1,52 @@
 @echo off
 setlocal enabledelayedexpansion
 
-echo --------------------------------------------------------------
-echo IPアドレス        サーバー名              状態
-echo --------------------------------------------------------------
+REM 疎通チェックバッチファイル
+REM 対象: DB, WEB, Engine x 2 のIP
 
-for %%i in (
-    "127.0.0.1" "DBサーバー" "8.8.8.8" "WebAPサーバー"
-    "1.1.1.1" "Engineサーバー#1" "1.0.0.1" "Engineサーバー#2"
-) do (
-    set "targetIP=%%~i"
-    ping -n 1 !targetIP! > nul
+echo +-------------------------------------------+---------------------------+
+echo ^|               サーバー名                  ^|          ステータス       ^|
+echo +-------------------------------------------+---------------------------+
 
-    if !errorlevel! equ 0 (
-        set "status=オンライン"
-    ) else (
-        set "status=オフラインか応答遅延"
+REM Pingを送信するIPアドレスとサーバー名
+set target[0]=127.127.127.127,DBサーバー
+set target[1]=8.8.8.8,WebAPサーバー
+set target[2]=1.1.1.1,Engineサーバー#1
+set target[3]=1.0.0.1,Engineサーバー#2
+
+for /L %%i in (0,1,3) do (
+    for /f "tokens=1,2 delims=," %%a in ("!target[%%i]!") do (
+        set "targetIP=%%a"
+        set "serverName=%%b"
+        
+        REM Pingを送信し、応答があるか確認
+        ping -n 1 !targetIP! > nul
+        if not errorlevel 1 (
+            set "status=オンライン"
+        ) else (
+            set "status=オフライン"
+        )
+        
+        REM サーバー名とステータスの文字列長を調整
+        call :padString "!serverName! IPアドレス !targetIP!" 33 serverOutput
+        call :padString "!status!" 20 statusOutput
+
+        echo ^| !serverOutput! ^| !statusOutput! ^|
+        echo +-------------------------------------------+---------------------------+
     )
-
-    echo !targetIP!        %%~j  !status!
 )
 
-echo --------------------------------------------------------------
 pause
 endlocal
+goto :eof
+
+:padString <inputString> <targetLength> <outputVariable>
+setlocal
+set "str=%~1"
+set "len=%~2"
+set "pad="
+for /l %%A in (1,1,%len%) do set "pad=!pad! "
+set "result=%str%!pad!"
+set "result=!result:~0,%len%!"
+endlocal & set "%~3=%result%"
+goto :eof
